8ec4b8ab2c3f5adbe820b25bdf1303e0d977c895
Nanshan Li <nanshan7596@gmail.com>
2019-01-30 15:26:19 -0500

    task1 fib test

--- task1/fib.py
+++ task1/fib.py
@@ -1,16 +1,15 @@
 def fib(n):
   prev = 1
   curr = 1
-  
+
   if n == 1:
     return 1
-  else if n == 2:
+  elif n == 2:
     return 1
-    
-  for i in range(3, n):
+
+  for i in range(3, n + 1):
     temp = prev + curr
     prev = curr
     curr = temp
-    
+
   return curr
-    
---
+++ task1/test_fib.py
@@ -0,0 +1,10 @@
+from fib import fib
+
+def test_two():
+    assert fib(2) == 1
+
+def test_five():
+    assert fib(5) == 5
+
+def test_twelve():
+    assert fib(12) == 144
