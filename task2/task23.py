141fb77f9d6b2b1cb81eb0b5ddf2abcc20b5bc8a
Nanshan Li <nanshan7596@gmail.com>
2019-02-03 10:47:35 -0500

    simplified code for task23

--- task2/task23.py
+++ task2/task23.py
@@ -47,28 +47,14 @@ def main():
         ix = np.where(drv == type)
         ax[0, 0].scatter(displ[ix[0]], cty[ix[0]],
                          c=cdict[index], label=drv_long[index], s=5)
-    ax[0, 0].legend(title="drive train")
-    ax[0, 0].set_ylabel('fuel economy (mpg)')
-    ax[0, 0].set_xlabel('displacement (l)')
-    ax[0, 0].set_xticks(np.arange(2, 7, 1))
-    ax[0, 0].spines['right'].set_color('white')
-    ax[0, 0].spines['top'].set_color('white')
     ax[0, 0].set_title('Figure 18.1')
-    plt.setp(ax[0, 0].get_legend().get_texts(), fontsize='8')
 
     # Plot 2
     for index, type in enumerate(drv_short):
         ix = np.where(drv == type)
         ax[0, 1].scatter(displ[ix[0]], cty[ix[0]],
                          c=cdict[index], label=drv_long[index], s=5, alpha=0.2)
-    ax[0, 1].legend(title="drive train")
-    ax[0, 1].set_ylabel('fuel economy (mpg)')
-    ax[0, 1].set_xlabel('displacement (l)')
-    ax[0, 1].set_xticks(np.arange(2, 7, 1))
-    ax[0, 1].spines['right'].set_color('white')
-    ax[0, 1].spines['top'].set_color('white')
     ax[0, 1].set_title('Figure 18.2')
-    plt.setp(ax[0, 1].get_legend().get_texts(), fontsize='8')
 
     # Plot 3
     displ_jitter = displ + np.random.randn(len(displ)) * 0.01 * (displ.max()
@@ -79,14 +65,7 @@ def main():
         ix = np.where(drv == type)
         ax[1, 0].scatter(displ_jitter[ix[0]], cty_jitter[ix[0]],
                          c=cdict[index], label=drv_long[index], s=5, alpha=0.2)
-    ax[1, 0].legend(title="drive train")
-    ax[1, 0].set_ylabel('fuel economy (mpg)')
-    ax[1, 0].set_xlabel('displacement (l)')
-    ax[1, 0].set_xticks(np.arange(2, 7, 1))
-    ax[1, 0].spines['right'].set_color('white')
-    ax[1, 0].spines['top'].set_color('white')
     ax[1, 0].set_title('Figure 18.3')
-    plt.setp(ax[1, 0].get_legend().get_texts(), fontsize='8')
 
     # Plot 4
     displ_xjitter = displ + np.random.randn(len(displ)) * (displ.max()
@@ -97,14 +76,17 @@ def main():
         ix = np.where(drv == type)
         ax[1, 1].scatter(displ_xjitter[ix[0]], cty_xjitter[ix[0]],
                          c=cdict[index], label=drv_long[index], s=5, alpha=0.2)
-    ax[1, 1].legend(title="drive train")
-    ax[1, 1].set_ylabel('fuel economy (mpg)')
-    ax[1, 1].set_xlabel('displacement (l)')
-    ax[1, 1].set_xticks(np.arange(2, 7, 1))
-    ax[1, 1].spines['right'].set_color('white')
-    ax[1, 1].spines['top'].set_color('white')
     ax[1, 1].set_title('Figure 18.4')
-    plt.setp(ax[1, 1].get_legend().get_texts(), fontsize='8')
+
+    for i in range(0, 2):
+        for j in range(0, 2):
+            ax[i, j].legend(title="drive train")
+            ax[i, j].set_ylabel('fuel economy (mpg)')
+            ax[i, j].set_xlabel('displacement (l)')
+            ax[i, j].set_xticks(np.arange(2, 7, 1))
+            ax[i, j].spines['right'].set_color('white')
+            ax[i, j].spines['top'].set_color('white')
+            plt.setp(ax[i, j].get_legend().get_texts(), fontsize='8')
 
     plt.tight_layout()
     plt.savefig('task2/task23.png')
